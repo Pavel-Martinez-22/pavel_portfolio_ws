7979a51bc7c1a32914a6df1ae5f6f2308bd6bb3c
Pavel Martinez <pavel.martinez.22@gmail.com>
2026-05-13 00:58:56 -0500

    renamed javaScript

---
+++ api.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", () => {
+  generateQuotes();
+  generateStates();
+});
+
+function generateQuotes() {
+  fetch("https://full-stack-api-and-random-quote-generator.onrender.com/api/quotes/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+function generateStates() {
+  fetch("https://full-stack-api-states.onrender.com/states/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
